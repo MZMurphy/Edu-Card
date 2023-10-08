367c3f0ee2044aebe9b112ef7a3ccbd2731a3239
Mariesha Murphy <mzaramurphy@gmail.com>
2023-10-08 12:20:20 +0100

    GUI complete

--- test.py
+++ test.py
@@ -1,47 +1,39 @@
 import tkinter as tk
+from tkinter import Canvas
 
 root = tk.Tk()
-root.title("EDU-CARD")
+root.title("Scalable Tkinter Example")
 
-# Make rows and columns resizable / Configure our rows and cols to be resizeable
-root.rowconfigure(0, weight=1)  # Make row 0 resizable
-root.rowconfigure(1, weight=1)  # Make row 1 resizable
-root.columnconfigure(index=0, weight=1)  # Make column 0 resizable
-root.columnconfigure(index=1, weight=1)  # Make column 1 resizable
-root.columnconfigure(index=2, weight=1)  # Make column 2 resizable
+# Create a label to display text
+text_label = tk.Label(root, text="Text Label")
+text_label.grid(row=1, column=0, columnspan=3, sticky='ew')
 
-# Create a buttons
-button1 = tk.Button(root, text="Button 1")
-button2 = tk.Button(root, text="Button 2")
-button3 = tk.Button(root, text="Button 3")
+# Create a canvas for displaying an image
+canvas = Canvas(root, bg="blue")
+canvas.grid(row=2, column=0, columnspan=3, sticky='nsew')
 
-# Could create another frame to stop button 2 from resizing with the canvas, but I actually like it better this way. 
+# Create a text input box
+input_box = tk.Entry(root)
+input_box.grid(row=3, column=0, columnspan=2, sticky='ew')
 
-# Grid buttons to root/screen 
-button1.grid(row=0, column=0, sticky="nsew")
-button2.grid(row=0, column=1, sticky="nsew")
-button3.grid(row=0, column=2, sticky="nsew")
+# Create a button for data submission
+submit_button = tk.Button(root, text="Submit") # Add function to button
+submit_button.grid(row=3, column=2, sticky='ew')
 
-# Set minimum size for rows and columns
-root.rowconfigure(0, minsize=50)  # Set the minimum height for row 0
-root.rowconfigure(1, minsize=50)  # Set the minimum height for row 1
-root.columnconfigure(0, minsize=100)  # Set the minimum width for column 0
-root.columnconfigure(1, minsize=100)  # Set the minimum width for column 1
-root.columnconfigure(2, minsize=100)  # Set the minimum width for column 2
+# Create three buttons in the top row and columns 1, 2, and 3
+button1 = tk.Button(root, text="Quit") # Add function to button
+button1.grid(row=0, column=0, sticky='ew')
+ 
+button2 = tk.Button(root, text="Open CSV") # Add function to button
+button2.grid(row=0, column=1, sticky='ew')
 
-# Create frame
-frame = tk.Frame(root, width=640, height=420)
+button3 = tk.Button(root, text="Next") # Add function to button
+button3.grid(row=0, column=2, sticky='ew')
 
-# Grid frame to root/screen
-frame.grid(row=1, column=1,) # Not an issue to use grid as not in same container as pack usage 
-
-# Create canvas
-canvas = tk.Canvas(frame, width=620, height=400, bg='blue')
-
-# Grid canvas to frame
-canvas.pack()
-
-# Create question box
-questionBox =tk.Label(text="") # Font not specified
+# Make the columns and rows expand when the window is resized
+for i in range(4):
+    root.grid_columnconfigure(i, weight=1)
+for i in range(4):
+    root.grid_rowconfigure(i, weight=1)
 
 root.mainloop()
--- test2.py
+++ test2.py
@@ -3,46 +3,55 @@ import tkinter as tk
 root = tk.Tk()
 root.title("EDU-CARD")
 
-# Make rows and columns resizable
-root.rowconfigure(0, weight=1)  # Make row 0 resizable
-root.rowconfigure(1, weight=1)  # Make row 1 resizable
-root.columnconfigure(0, weight=1)  # Make column 0 resizable
-root.columnconfigure(1, weight=1)  # Make column 1 resizable
-root.columnconfigure(2, weight=1)  # Make column 2 resizable
+# Make frame for buttons and grid to root
+buttonFrame = tk.Frame(root)
+buttonFrame.grid(row=0, column=0)
 
-# Create buttons
-button1 = tk.Button(root, text="Button 1")
-button2 = tk.Button(root, text="Button 2")
-button3 = tk.Button(root, text="Button 3")
+# Config root rows and cols to be resizeable
+root.rowconfigure(index=0, weight=1)  # Make row 0 resizable
+root.rowconfigure(index=1, weight=1) 
+root.columnconfigure(index=0, weight=1)  # Make column 0 resizable
 
-# Grid buttons to root/screen
+# Config buttonFrame rows and cols to be resizeable
+buttonFrame.rowconfigure(index=0, weight=1)
+buttonFrame.columnconfigure(index=0, weight=1)
+buttonFrame.columnconfigure(index=1, weight=1)
+buttonFrame.columnconfigure(index=2, weight=1)
+
+# Create a buttons
+button1 = tk.Button(buttonFrame, text="Button 1")
+button2 = tk.Button(buttonFrame, text="Button 2")
+button3 = tk.Button(buttonFrame, text="Button 3")
+
+# Grid buttons to buttonFrame 
 button1.grid(row=0, column=0, sticky="nsew")
 button2.grid(row=0, column=1, sticky="nsew")
 button3.grid(row=0, column=2, sticky="nsew")
 
 # Set minimum size for rows and columns
 root.rowconfigure(0, minsize=50)  # Set the minimum height for row 0
+root.rowconfigure(1, minsize=50)  # Set the minimum height for row 1
 root.columnconfigure(0, minsize=100)  # Set the minimum width for column 0
 root.columnconfigure(1, minsize=100)  # Set the minimum width for column 1
 root.columnconfigure(2, minsize=100)  # Set the minimum width for column 2
 
-# Create frame for canvas
-frame = tk.Frame(root)
-frame.grid(row=1, column=1, sticky="nsew")
+# Create canvasframe and grid to root
+canvasframe = tk.Frame(root)
+canvasframe.grid(row=1, column=0, sticky="nsew") # Not an issue to use grid as not in same container as pack usage 
 
-# Create canvas within the frame
-canvas = tk.Canvas(frame, bg='blue')
+# Set the maximum size for the canvas based on the size of canvasframe
+canvasframe.grid_rowconfigure(0, weight=1)
+canvasframe.grid_columnconfigure(0, weight=1)
+
+# Create canvas and pack to cavasFrame
+canvas = tk.Canvas(canvasframe, bg='blue')
 canvas.grid(row=0, column=0, sticky="nsew")
 
-# Set maximum size for the canvas
-#canvas.create_rectangle(0, 0, 620, 400)  # Set the maximum size for the canvas
-canvas.create_rectangle(0, 0, frame.winfo_reqwidth(), frame.winfo_reqheight())  # Set the maximum size for the canvas
+# Create a rectangle that fills the canvas
+canvas.create_rectangle(0, 0, canvas.winfo_reqwidth(), canvas.winfo_reqheight(), fill="red")  # Set the maximum size for the canvas
 
 # Set minimum size for rows and columns in the frame
-frame.rowconfigure(0, weight=1)
-frame.columnconfigure(0, weight=1)
-
-# Create question box
-questionBox = tk.Label(text="")  # Font not specified
+canvasframe.rowconfigure(0, weight=1)
+canvasframe.columnconfigure(0, weight=1)
 
 root.mainloop()
